b6718ee0c3c8e52d9cff36c7a6864faf4bb63485
jljimenezc <joseluis.jimenezcevallos@gmail.com>
2025-06-13 23:36:12 -0500

    Agregar endpoint para obtener posts por autor
    - Se creó la ruta GET /api/autores/:id/posts para recuperar todos los posts de un autor específico.
    - Se implementó la consulta en el controlador usando el ID del autor desde la URL.

--- BlogExpress/src/routes/api/autores.routes.js
+++ BlogExpress/src/routes/api/autores.routes.js
@@ -1,8 +1,16 @@
-const { getAll } = require('../../controllers/autores.controller');
+const {
+  getAll,
+  create,
+  getById,
+  getPostsByAutorId,
+} = require("../../controllers/autores.controller");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Define your routes here
-router.get("/", getAll)
+router.get("/", getAll);
+router.get("/:autorId", getById);
+router.get("/:autorId/posts", getPostsByAutorId);
+router.post("/", create);
 
 module.exports = router;
