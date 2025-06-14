a57ac8d9931f128ca2b4fb1ec5e82a07d86b0591
jljimenezc <joseluis.jimenezcevallos@gmail.com>
2025-06-13 22:41:32 -0500

    Creación de endpoints para crear y recuperar posts
    - Se creó la ruta para insertar nuevos posts en la base de datos.
    - Se implementó la ruta para recuperar todos los posts.
    - Se agregó la ruta para recuperar un post específico por su ID.

--- BlogExpress/src/controllers/autores.controller.js
+++ BlogExpress/src/controllers/autores.controller.js
@@ -1,5 +1,13 @@
-const getAll = (req, res) => {
-    // Implement your logic here
-}
+const { query } = require("../config/db");
+const Autores = require("../models/autores.model");
 
-module.exports = { getAll }
+const getAll = async (req, res) => {
+  // Implement your logic here
+  const autores = await Autores.selectAll()
+
+  res.json({
+    results: autores,
+  });
+};
+
+module.exports = { getAll };
--- BlogExpress/src/routes/api/posts.routes.js
+++ BlogExpress/src/routes/api/posts.routes.js
@@ -1,5 +1,10 @@
+const { getAll, create, getById } = require('../../controllers/posts.controller');
+
 const router = require('express').Router();
 
 // Define your routes here
+router.get("/", getAll)
+router.get("/:postId", getById)
+router.post("/", create)
 
 module.exports = router;
